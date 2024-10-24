e9e1cdb859e8b914e3a16da1f911dd4c75e6120f
JorshGL <jorge.gl0905@gmail.com>
2024-10-24 13:01:05 -0500

    configure theme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    // <IonPage>
-    //   <IonHeader>
-    //     <IonToolbar>
-    //       <IonTitle>Buenos días</IonTitle>
-    //     </IonToolbar>
-    //   </IonHeader>
-    //   <IonContent fullscreen>
-    //     <IonHeader collapse="condense">
-    //       <IonToolbar>
-    //         <IonTitle size="large">Blank</IonTitle>
-    //       </IonToolbar>
-    //     </IonHeader>
-    //     <ExploreContainer />
-    //   </IonContent>
-    // </IonPage>
-    <div>
-      <h1 className='font-mono text-blue-600'>Home</h1>
-      <p>This is the Home page.</p>
-    </div>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Buenos días</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+        <IonButton>Login</IonButton>
+      </IonContent>
+    </IonPage>
+    // <div>
+    //   <h1 className='font-mono text-blue-600'>Home</h1>
+    //   <p>This is the Home page.</p>
+    // </div>
   );
 };
 
